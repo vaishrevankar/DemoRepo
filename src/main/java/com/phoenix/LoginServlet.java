6d2e8fad171fc80d8feda5d383a95c39967dd190
soumya <55653149+soumyashetagar@users.noreply.github.com>
2019-10-16 19:36:53 +0530

    Update LoginServlet.java

--- src/main/java/com/phoenix/LoginServlet.java
+++ src/main/java/com/phoenix/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.phoenix.Check;
+
 
 /**
  * Servlet implementation class LoginServlet
